a886715b012b31c2850c0f393060e2e59f06befb
dasaradhi <dasaradhinaidu45@gmail.com>
2026-03-28 22:29:05 +0530

    9

---
+++ week9/readline.py
@@ -0,0 +1,11 @@
+try:
+    file = open("sample.txt","r")
+    temp1=file.readline()
+    temp2=file.readline()
+    temp3=file.readline()
+
+    print(temp1)
+    print(temp2)
+    print(temp3)
+except FileNotFoundError:
+    print("File does not exist..")
